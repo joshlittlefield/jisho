055ba0b12554ecbb112006f2d9e6c20dac3e82e7
Josh LITTLEFIELD <josh@dreamdojo.com>
2021-03-05 08:30:23 +0000

    changed kanji.py

--- models/Kanji.py
+++ models/Kanji.py
@@ -8,28 +8,20 @@ class Kanji(db.Model):
     __tablename__ = "kanjis"
     
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200),nullable=False)
-    content = db.Column(db.String(30),nullable=False)
-    genre = db.Column(db.String(),default='Dont know what genre to call it')
-    date_created = db.Column(db.DateTime,default=datetime.utcnow,nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
-    
-    
-    
     heisig_level=db.Column(db.Integer)
     jlpt_level=db.Column(db.Integer)
     school_level=db.Column(db.Integer)
-    character=
-    meaning=
-    onyomi=
-    kunyomi=
-    is_radical=
-    R1_radical_used=
-    R2_radical_used=
-    R3_radical_used=
-    R4_radical_used=
-    R5_radical_used=
-    story=
+    character=db.Column(db.String(200),nullable=False)
+    meaning=db.Column(db.String(200),nullable=False)
+    onyomi=db.Column(db.String(200),nullable=False)
+    kunyomi=db.Column(db.String(200),nullable=False)
+    is_radical=db.Column(db.String(200))
+    R1_radical_used=db.Column(db.String(200))
+    R2_radical_used=db.Column(db.String(200))
+    R3_radical_used=db.Column(db.String(200))
+    R4_radical_used=db.Column(db.String(200))
+    R5_radical_used=db.Column(db.String(200))
+    story=db.Column(db.String(200))
     
     
     
