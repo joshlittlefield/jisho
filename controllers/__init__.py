85c483c4b0e21c3badcbf5ec6174952ee5876651
Josh LITTLEFIELD <josh@dreamdojo.com>
2021-03-05 09:48:14 +0000

    kanji shemas and controllers

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,8 +1,10 @@
+from controllers.kanjis_controller import kanjis
 from controllers.suggestions_controller import suggestions
 from controllers.auth_controller  import auth
 
 
 registerable_controllers = [
     auth,
-    suggestions
+    suggestions,
+    kanjis
 ]
--- schemas/KanjiSchema.py
+++ schemas/KanjiSchema.py
@@ -1,11 +1,11 @@
 from main import ma
-from models.Suggestion import Suggestion
+from models.Kanji import Kanji
 from marshmallow.validate import Length
 from schemas.UserSchema import UserSchema
 
-class SuggestionSchema(ma.SQLAlchemyAutoSchema):
+class KanjiSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
-        model = Suggestion
+        model = Kanji
 
     video_title = ma.String(required=True, validate=Length(min=1))
     video_type = ma.String(required=True, validate=Length(min=1))
@@ -13,5 +13,5 @@ class SuggestionSchema(ma.SQLAlchemyAutoSchema):
     date_created = ma.DateTime(required=True)
     user =  ma.Nested(UserSchema)
     
-suggestion_schema = SuggestionSchema()
-suggestions_schema = SuggestionSchema(many=True)
+kanji_schema = KanjiSchema()
+kanjis_schema = KanjiSchema(many=True)
