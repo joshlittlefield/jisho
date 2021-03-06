e3cfe746aaea330ccb581cef15e0485068071f5f
Josh LITTLEFIELD <josh@dreamdojo.com>
2021-03-06 10:36:37 +0000

    kanji

--- controllers/kanjis_controller.py
+++ controllers/kanjis_controller.py
@@ -17,6 +17,6 @@ kanjis = Blueprint('kanjis', __name__)
 @login_required
 def kanji():
     # Retrieve all suggestions
-    kanjis = Kanji.query.options(joinedload("user")).all()
+    kanjis = Kanji.query.options.get_or_404(suggestion_id)
     return render_template("kanji.html", kanjis=kanjis)
     
