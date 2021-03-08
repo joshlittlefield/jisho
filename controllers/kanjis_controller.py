0100e12377a7581143246cd3dced26e928740e37
Josh LITTLEFIELD <josh@dreamdojo.com>
2021-03-08 04:52:42 +0000

    heisig variable

--- controllers/kanjis_controller.py
+++ controllers/kanjis_controller.py
@@ -31,6 +31,6 @@ def single_kanji(kanji_id):
 @kanjis.route("/heisig_level/<int:heisig_level>")
 def single_heisig_level(single_heisig_level):
     
-    kanjis = Kanji.query.filter_by(heisig_level=single_heisig_level)
+    kanjis = Kanji.query.filter_by(heisig_level=single_heisig_level).all()
     return render_template('single_heisig_level.html', kanjis=kanjis)
     
